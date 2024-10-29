5fce4aca7edffe25dbb95ea0ecda1b28c7aeedfc
Alexabet Villasmil <134399186+alexabetv@users.noreply.github.com>
2024-10-29 20:22:18 -0300

    Add files via upload

---
+++ conexion_be.php
@@ -0,0 +1,9 @@
+<?php
+    $conexion = myslqi_connect("localhost", "root", "", "login_register_db");
+
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+?>
